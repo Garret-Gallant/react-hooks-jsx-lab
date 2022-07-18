5024aa07281d432bbba6afea6e17721a1e5a9ce6
Garret Gallant <gallanttechnological@gmail.com>
2022-07-18 16:33:05 -0600

    lesson complete

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,19 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return ( 
+  <div id='about'>
+    <h2>About Me</h2>
+    <p>
+    I'm from a small mountain town in Colorado. My previous experience includes design and 
+    implementation of large-scale commercial audio visual systems, and cybersecurity threat analysis/asset management for 
+    fortune 500 companies around the globe. From a very young age I've been inspired to innovate
+    and inspire positive change in the quality of everyday life using technology as my medium. I pride myself in my ability 
+    to inspire teamwork, communicate well, and bring strong positive energy into every role I take. 
+    </p>
+    <img src = {image} alt='I made this.'/>
+  </div>
+  )
 }
 
 export default About;
